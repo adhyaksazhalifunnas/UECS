4445faa88d85a04e3a72d4c04b082ce8489e9e66
Adhyaksa Zhalifunnas <adhyaksazhalifunnas@gmail.com>
2021-04-29 05:54:20 +0700

    Update queuebase.sql
    Add new table `useradmin`

--- queuebase.sql
+++ queuebase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Apr 2021 pada 00.52
+-- Waktu pembuatan: 29 Apr 2021 pada 00.53
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 8.0.3
 
@@ -33,6 +33,28 @@ CREATE TABLE `tr_guest` (
   `Admin` varchar(50) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `useradmin`
+--
+
+CREATE TABLE `useradmin` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(100) COLLATE utf8_bin NOT NULL,
+  `emailaddress` text COLLATE utf8_bin NOT NULL,
+  `username` varchar(100) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data untuk tabel `useradmin`
+--
+
+INSERT INTO `useradmin` (`id`, `firstname`, `lastname`, `emailaddress`, `username`, `password`) VALUES
+(1, 'User', 'Admin', 'useradmin@sample.com', 'admin', '123');
+
 --
 -- Indexes for dumped tables
 --
@@ -44,6 +66,12 @@ ALTER TABLE `tr_guest`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `useradmin`
+--
+ALTER TABLE `useradmin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -52,6 +80,12 @@ ALTER TABLE `tr_guest`
 --
 ALTER TABLE `tr_guest`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `useradmin`
+--
+ALTER TABLE `useradmin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
